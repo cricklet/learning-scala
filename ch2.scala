52b96e1cc7eb92adf02ddca47d346600feff1a05
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-03-10 20:58:58 -0800

    Tail-call elimination

--- ch2.scala
+++ ch2.scala
@@ -9,9 +9,21 @@ object Maths {
   }
 
   def factorial(n: Int): Int = {
+
+    // Tail call elimination: when 'self-recursion' is compiled into the same sort
+    // of byte-code as would be emitted for a `while` loop.
+
+    // Goodbye StackOverflowError!
+
+    // Note: sometimes tail call elimination isn't successful. To find out when 'scalac'
+    // fails to eliminate a tail call, use 'tailrec.'
+
+    @annotation.tailrec
     def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
-      else go(n - 1, n * acc)
+      else
+        // A call is in 'tail-position' if it simply returns the value of a recursive call.
+        go(n - 1, n * acc)
 
     go(n, 1)
   }
