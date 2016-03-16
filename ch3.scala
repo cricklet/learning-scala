4ed9badbd3e26509dede467e8a4f330fd1074016
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-03-16 08:25:48 -0700

    Pattern matching can involve nested functions :O

--- ch3.scala
+++ ch3.scala
@@ -64,6 +64,16 @@ val animalList: List[Animal] = Cons(new Dog(), Nil)
   //  case Person("Barak") => "wooo"
   //  case _ => "awww"
   //}
+
+  // Let's try a weird one from the book
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+  println("I expect %d to be: 1 + 2 = 3".format(x))
 }
 
 {
