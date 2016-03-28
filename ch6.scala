d67700333fefdd95716beaa07f66a71c5d07b9ee
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-03-27 23:02:09 -0700

    Generalizing Rand to State

--- ch6.scala
+++ ch6.scala
@@ -1,3 +1,5 @@
+import State._
+
 // Traits are like interfaces from Java. They can be partially implemented
 // (i.e. like an abstract class)
 
@@ -160,3 +162,30 @@ println("map2ViaFlatMap: %s".format(
   map2ViaFlatMap(randInt, randInt)
   ((x, y) => (x % 100, y % 100))(rng2)
 ))
+
+// Our type 'Rand' can be generalized to any kind of 'State':
+// i.e. type State[S,+A] = S => (A,S)
+//      type Rand[A] = State[RNG, A]
+
+case class State[S,+A](run: S => (A, S)) {
+  def flatMap [B](g: A => State[S,B]): State[S,B] =
+    State(state0 => {
+      val (a, state1) = run(state0)
+      g(a).run(state1)
+    })
+
+  def map [B] (f: A => B): State[S,B] =
+    flatMap(a => unit(f(a)))
+
+  def map2 [B, C] (other: State[S,B])(f: (A, B) => C): State[S,C] =
+    flatMap(a => {
+      other.flatMap(b => {
+        State(s => (f(a, b), s))
+      })
+    })
+}
+
+object State {
+  def unit [S,A] (a: A): State[S,A] =
+    State(state => (a, state))
+}
