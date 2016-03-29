29ab4aa2d9483f8d7372bbd8f2702fba49be4836
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-03-28 18:07:52 -0700

    Un-nesting a series of flatMaps/maps using for-comprehensions!

--- ch6-state.scala
+++ ch6-state.scala
@@ -1,8 +1,7 @@
+
 // Our type 'Rand' (in ch6-rand.scala) can be generalized to any kind of 'State'
 // i.e. type State[S,+A] = S => (A,S)
 //      type Rand[A] = State[RNG, A]
-
-import State._
 
 case class State[S,+A](run: S => (A, S)) {
   def flatMap [B](g: A => State[S,B]): State[S,B] =
@@ -12,7 +11,7 @@ case class State[S,+A](run: S => (A, S)) {
     })
 
   def map [B] (f: A => B): State[S,B] =
-    flatMap(a => unit(f(a)))
+    flatMap(a => State.unit(f(a)))
 
   def map2 [B, C] (other: State[S,B])(f: (A, B) => C): State[S,C] =
     flatMap(a => {
@@ -25,4 +24,53 @@ case class State[S,+A](run: S => (A, S)) {
 object State {
   def unit [S,A] (a: A): State[S,A] =
     State(state => (a, state))
+
+  def modify [S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  def get [S]: State[S,S] = State(s => (s, s))
+  def set [S](s: S): State[S, Unit] = State(_ => ((), s))
 }
+
+// Let's implement a very simple state machine that counts:
+def counter: State[Int, Int] =
+  State((s: Int) => (s + 1, s + 1))
+
+val (v0, s0) = counter.run(0)
+val (v1, s1) = counter.run(s0)
+val (v2, s2) = counter.run(s1)
+val (v3, s3) = counter.run(s2)
+println("Count from 0: %s, %s, %s, %s".format(v0, v1, v2, v3))
+
+// Because State has 'map' and 'flatMap' defined, we can use it in
+// for-comprehensions! Let's try counting using a for-comprehensions:
+val count0: State[Int, Int] = for {
+  x0 <- counter
+  x1 <- counter
+  x2 <- counter
+  x3 <- counter
+} yield x3
+
+// This code, roughly translates to:
+val count1: State[Int, Int] =
+  counter.flatMap(x0 =>
+    counter.flatMap(x1 =>
+      counter.flatMap(x2 =>
+        counter.map(x3 => x3))))
+
+println("Using a for-comprehension to count: %s".format(count0.run(0)._1))
+println("Replicating that code using nested flatMaps + map: %s".format(count1.run(0)._1))
+
+// To better understand this, let's unroll a simpler loop:
+val l0: List[(Int, Int)] = for {
+  x <- List(0,1,2)
+  y <- List(9,8)
+} yield (x, y)
+
+val l1: List[(Int, Int)] = List(0,1,2).flatMap(x =>
+  List(9,8).map(y => (x, y)))
+
+println("Using a for-comprehension to combine two lists: %s".format(l0))
+println("Using a nested flatMaps + map to combine two lists: %s".format(l1))
