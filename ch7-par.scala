ff97e04301c2a1ea5bdee0284a9239a5e40a685d
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-04-05 19:06:37 -0700

    Figuring out the types

--- ch7-par.scala
+++ ch7-par.scala
@@ -1,18 +1,47 @@
 
+import java.util.concurrent._
+
+// Here's my first attempt to come up with a representation for Par...
+// sealed trait Par[+A]
+// case class ForkedPar [+A] (a: () => A) extends Par[A]
+// case class SimplePar [+A] (a: A) extends Par[A]
+
+// Oh, actually, we're going to use ExecutorService, Callable, & Future.
+
+// trait Future [A] { def get: A }
+// trait Callable [A] { def call: A }
+// class ExecutorService {
+//   def submit [A] (a: Callable[A]): Future[A]
+// }
+
+// So, an ExecutorService turns a Callable into a Future.
+// I think that we'll want to embed a:A into a Callable[A] and return a function
+// which lets you execute that callable and turn it into a value
+
+// type Par[A] = ExecutorService => A
+
+// The book points out that returning a Future[A] is more flexible as it gives
+// caller the ability to decide how long to wait for a computation, whether to
+// cancel it, etc.
+
+// This changes a little of the API we outlined earlier. run() must now return
+// a Future.
+type Par[A] = ExecutorService => Future[A]
+
 object Par {
   // Promotes a constant value to a parallel computation
-  def unit [A] (a: A): Par[A]
+  // def unit [A] (a: A): Par[A]
 
   // Combine the result of two parallel computations with 'f'
-  def map2 [A, B, C] (a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+  // def map2 [A, B, C] (a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
 
   // Flag a computation for concurrent evaluation
-  def fork [A] (a: Par[A]): Par[A]
+  // def fork [A] (a: Par[A]): Par[A]
 
   // Promote a thunk value to a parallel compuation
   // & mark it for concurrent evaluation
-  def lazyUnit [A] (a: => A): Par[A] =
+  // def lazyUnit [A] (a: => A): Par[A] =
 
   // Extract a value from a Par by actually performing the computation
-  def run [A] (a: Par[A]): A
+  // def run [A] (a: Par[A]): Future[A]
 }
