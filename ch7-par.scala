db9cb4af83241a003c1f26827356fb5616c4472b
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-04-05 19:19:44 -0700

    Starting to implement the basic 'Par' utilities

--- ch7-par.scala
+++ ch7-par.scala
@@ -29,18 +29,41 @@ import java.util.concurrent._
 type Par[A] = ExecutorService => Future[A]
 
 object Par {
+  private class UnitFuture [A] (get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit) = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+    def get() = get
+  }
+
   // Promotes a constant value to a parallel computation
-  // def unit [A] (a: A): Par[A]
+  def unit [A] (a: A): Par[A] =
+    (e: ExecutorService) => new UnitFuture(a)
 
   // Combine the result of two parallel computations with 'f'
-  // def map2 [A, B, C] (a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+  def map2 [A, B, C] (a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (e: ExecutorService) => {
+      val aFuture = a(e)
+      val bFuture = b(e)
+      new UnitFuture(f(aFuture.get, bFuture.get))
+    }
 
   // Flag a computation for concurrent evaluation
-  // def fork [A] (a: Par[A]): Par[A]
+  def fork [A] (a: Par[A]): Par[A] =
+    // Given an executor
+    (e: ExecutorService) => {
+      // Submit a new job
+      e.submit(new Callable[A] {
+        // That runs the parallel computation 'a' using that executor
+        def call: A = a(e).get
+      })
+    }
 
   // Promote a thunk value to a parallel compuation
   // & mark it for concurrent evaluation
-  // def lazyUnit [A] (a: => A): Par[A] =
+  def lazyUnit [A] (a: => A): Par[A] =
+    fork(unit(a))
 
   // Extract a value from a Par by actually performing the computation
   // def run [A] (a: Par[A]): Future[A]
