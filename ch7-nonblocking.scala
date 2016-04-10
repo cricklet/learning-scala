35b26a3e4d9207f4be4aef04cb0579b5cbf65eee
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-04-10 12:15:30 -0700

    Fork with callback

--- ch7-nonblocking.scala
+++ ch7-nonblocking.scala
@@ -13,7 +13,24 @@ object Nonblocking {
   type Par[+A] = ExecutorService => Future[A]
 
   object Par {
-    def run [A] (es: ExecutorService)(p: Par[A]): A = {
+    def unit[A](a: A): Par[A] =
+      (es: ExecutorService) => new Future[A] {
+        def apply(cb: A => Unit): Unit =
+            cb(a)
+      }
+
+    def eval(es: ExecutorService)(r: => Unit): Unit =
+      es.submit(new Callable[Unit] { def call = r })
+
+    def fork[A](p: => Par[A]): Par[A] =
+      (es: ExecutorService) => new Future[A] {
+        def apply(cb: A => Unit): Unit =
+          eval(es)(
+            p(es) { a => cb(a) }
+          )
+      }
+
+    def run[A](es: ExecutorService)(p: Par[A]): A = {
       val ref = new AtomicReference[A] // Mutable, threadsafe reference
       val latch = new CountDownLatch(1) // Wait for 'latch' to be called once
       p(es) { a => { ref.set(a); latch.countDown() } }
