0e4a12df9ec93823721b3b3393c4874a4f6a743d
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-04-10 12:08:44 -0700

    Start filling out our non-blocking Par. It exposes a 'run' command that's blocking

--- ch7-nonblocking.scala
+++ ch7-nonblocking.scala
@@ -4,13 +4,21 @@ import java.util.concurrent.atomic.AtomicReference
 import language.implicitConversions
 
 object Nonblocking {
-  sealed trait Future[A] {
-    private def apply(k: A => Unit): Unit
+  trait Future[+A] {
+    // We need this function to be private because it is not functionally pure.
+    // If we want our API to be functionally pure, we can't have this be public!
+    private[Nonblocking] def apply(k: A => Unit): Unit
   }
 
   type Par[+A] = ExecutorService => Future[A]
 
   object Par {
-
+    def run [A] (es: ExecutorService)(p: Par[A]): A = {
+      val ref = new AtomicReference[A] // Mutable, threadsafe reference
+      val latch = new CountDownLatch(1) // Wait for 'latch' to be called once
+      p(es) { a => { ref.set(a); latch.countDown() } }
+      latch.await() // Wait for result...
+      ref.get()
     }
+  }
 }
