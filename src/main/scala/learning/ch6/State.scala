5a74167a8ee0e4e4d4d8074980d5da47b130e576
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-04-27 21:20:21 -0700

    State.scala

---
+++ src/main/scala/learning/ch6/State.scala
@@ -0,0 +1,50 @@
+package learning.ch6
+
+case class State[S,+A](run: S => (A, S)) {
+  def flatMap [B](g: A => State[S,B]): State[S,B] =
+    State(state0 => {
+      val (a, state1) = run(state0)
+      g(a).run(state1)
+    })
+
+  def map [B] (f: A => B): State[S,B] =
+    flatMap(a => State.unit(f(a)))
+
+  def map2 [B, C] (other: State[S,B])(f: (A, B) => C): State[S,C] =
+    flatMap(a => {
+      other.flatMap(b => {
+        State(s => (f(a, b), s))
+      })
+    })
+}
+
+object State {
+  def unit [S,A] (a: A): State[S,A] =
+    State(state => (a, state))
+
+  def sequence [S,A] (l: List[State[S,A]]): State[S,List[A]] =
+    l.foldRight(unit[S,List[A]](Nil))((next: State[S,A], prev: State[S,List[A]]) =>
+      next.map2(prev)(_ :: _)
+    )
+
+  def modify [S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
+  def get [S]: State[S,S] = State(s => (s, s))
+  def set [S](s: S): State[S, Unit] = State(_ => ((), s))
+}
+
+trait RNG {
+  def nextInt: (Int, RNG) // Return both the random int AND a new RNG
+}
+
+case class SimpleRNG (seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
--- src/main/scala/learning/ch8/PropertyTesting.scala
+++ src/main/scala/learning/ch8/PropertyTesting.scala
@@ -1,14 +1,20 @@
 package learning.ch8
 
+
 object PropertyTesting {
   trait Prop {
-    def &&(p: Prop): Prop
-    def check(): Unit
+    def &&(p: Prop): Prop = {
+      lazy val checked = p.check() && this.check()
+      new Prop { def check() = checked }
+    }
+    def check(): Boolean
   }
 
-  trait Gen[A] {
-    def listOf[A](gen: Gen[A]): Gen[List[A]]
-    def choose(low: Int, high: Int): Gen[Int]
-    def forAll[A](gen: Gen[A])(f: A => Boolean): Boolean
+  trait Gen[A] {}
+  case class Gen[A](sample: State[RNG, A])
+  object Gen {
+    // def listOf[A](gen: Gen[A]): Gen[List[A]]
+    // def choose(low: Int, high: Int): Gen[Int]
+    // def forAll[A](gen: Gen[A])(f: A => Boolean): Boolean
   }
 }
