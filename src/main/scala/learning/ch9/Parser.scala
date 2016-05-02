65ec14a9b3b867fffd0e280e0d760ff679366e11
Kenrick Rilee <kenrick.rilee@gmail.com>
2016-05-02 15:02:28 -0700

    Fuck it, I'm skipping the rest of Part 2. I'm not motivated by it.

--- src/main/scala/learning/ch9/Parser.scala
+++
@@ -1,9 +0,0 @@
-package learning.ch9
-
-trait ParseError
-trait Parser[A]
-
-trait Parsers[ParserError, Parser[+_]] {
-  def char(c: Char): Parser[Char]
-  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-}
